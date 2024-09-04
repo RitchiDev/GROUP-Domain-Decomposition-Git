1c0dc63dd63d491d1313c8812bc7fc389298452d
Wesley de Graaf <double.gezicht@gmail.com>
2024-09-04 23:50:33 +0200

    UML Draft + Extra Powerup Info

--- Breakout Decomposition.cs
+++ Breakout Decomposition.cs
@@ -1,5 +1,7 @@
 /*
-Breakout Decomposition
+DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT 
+
+Breakout Decomposition 
 Andrich & Wesley
 
 Andrich: Bricks
@@ -16,3 +18,33 @@ Powerups:
 In Breakout there are multiple powerups: Laser, Enlarge, Catch, Slow, Break, Disruption & Player (From Arkanoid Game)
 When breaking a brick there is a x amount of chanche a powerup will spawn. When collecting the powerup. One of the powerups will happen for an x ammount of time.
 Some powerups are stackable (with a max size).
+
+Powerups:
+    Laser: Enables a laser fromt he paddle that destroys bricks.
+    Enlarge: Increases the size of the paddle.
+    Catch: Allows the paddle to catch and hold the ball temporarily.
+    Slow: Reduces the speed of the balls.
+    Break: Instantly destroys a certain number of bricks upon activation.
+    Disruption: Splits the ball into multiple balls.
+    Player: Gives the player an extra life.
+
+UML Draft:
+
+class PowerUp:
+    Vector2 position;
+    float duration;
+    bool stackable;
+
+Methods:
+    protected virtual void Update();
+    public virtual void Activate();
+    protected virtual void Deactivate();
+
+class Enlarge : Powerups:
+    float enlargementFactor;
+
+class Slow : Powerups:
+    float speedReduction;
+
+class Disruption : Powerups:
+    int ballMultiplier;
