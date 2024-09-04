3f64c67a8474ce46465da5b985e040f090a2ccf7
Wesley de Graaf <double.gezicht@gmail.com>
2024-09-04 14:28:46 +0200

    Powerup description

--- Breakout Decomposition.cs
+++ Breakout Decomposition.cs
@@ -8,3 +8,6 @@ Wesley: Powerups
 Bricks:
 
 Powerups:
+In Breakout there are multiple powerups: Laser, Enlarge, Catch, Slow, Break, Disruption & Player (From Arkanoid Game)
+When breaking a brick there is a x amount of chanche a powerup will spawn. When collecting the powerup. One of the powerups will happen for an x ammount of time.
+Some powerups are stackable (with a max size).
