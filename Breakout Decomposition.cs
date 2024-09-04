5cb8fcc77aca641153667394b6460ce5576e1e42
Andrich Laker <andrichlaker@gmail.com>
2024-09-04 14:25:51 +0200

    - Bricks first commit

--- Breakout Decomposition.cs
+++ Breakout Decomposition.cs
@@ -6,5 +6,10 @@ Andrich: Bricks
 Wesley: Powerups
 
 Bricks:
+Bricks/Obstacles have different variants: Brick, Silver, Gold.
+Bricks can be damaged/broken by being hit by the ball.
+Bricks break after 1 hit. Silver bricks break after 3 hits. Gold bricks break after 5 hits.
+The more hits it takes to break a brick the more points you gain after breaking one.
+These bricks sometimes drop a powerup after being broken.
 
 Powerups:
